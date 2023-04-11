9f60ad6c303807a98bd2e02661342088810b9056
WillW7 <william.wright03@gmail.com>
2023-04-11 21:27:41 +0100

    removed code and added instructions/hints

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
+// adapted from https://github.com/raspberrypi/pico-examples/blob/master/picoboard/blinky/blinky.c
+// also a solution ^
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define BUZZER 18
+#define BUZZER 18 //pin out of buzzer on maker board
 
 #define PAUSE 100 //defines delay between dots/dashes
 #define PAUSE_CHAR 300 //defines delay between each letter of a word (3x PAUSE)
 #define PAUSE_WORD 700 //defines delay between each word (7x PAUSE)
 
-char str[] = "Hello, World!";
+char str[] = "Text to translate"; //change this to whatever you want
 char* ptr = str;
 
 const char *morse_chars[] = {
@@ -50,28 +53,21 @@ void dot() {
     sleep_ms(PAUSE);      
 }
 
-void dash() {
-    for(int i=0;i<150;i++) // must last 3 times as dot
-        {
-            gpio_put(BUZZER,1);
-            sleep_ms(1);
-            gpio_put(BUZZER,0);
-            sleep_ms(1);
-        }
-    sleep_ms(PAUSE);
+void dash() { 
+    
+    // implement the dash
+    // usually 3 times dot
+
 }
 
-void play_morse_char(const char *morse) {
-    for (; *morse; ++morse)
-    {
-        if (*morse == '.') {
-            dot();
-        } else if (*morse == '-') {
-            dash();
-        }
-    }
-    sleep_ms(PAUSE_CHAR);
+void play_morse_char(const char *letter) {
+    
+    // iterate through the morse pattern passed
+    // calling dot()/dash() accordingly
+    // pausing after letter is completed
+
 }
+
 
 int main() {
     stdio_init_all();
@@ -81,13 +77,12 @@ int main() {
 
     for (; *ptr; ++ptr)
     {   
-        if (*ptr >= 'A' && *ptr <= 'Z') {
-            play_morse_char(morse_chars[*ptr - 'A']);
-        } else if (*ptr >= 'a' && *ptr <= 'z') {
-            play_morse_char(morse_chars[*ptr-'a']);
-        } else if (*ptr == ' ') {
-            sleep_ms(PAUSE_WORD);
-        }
+        
+        // iterate through string
+        // calling play_morse_char(morse_char[index]) on the corresponding letter
+        // hint: use assci value and index accordingly
+        // handle uppercase, lowercase, spaces and other symbols
+
     }
     
 }
